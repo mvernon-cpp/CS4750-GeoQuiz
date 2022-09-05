b339c82add90a3468ad9d086ac18ccfa4db0436d
Moina Vernon <mvernon@cpp.edu>
2022-09-04 20:59:13 -0700

    Commit GeoQuiz Challenge CH2

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -3,19 +3,23 @@ package com.bignerdranch.android.geoquiz
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
-import android.util.Log
 //import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-
-private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+//    private lateinit var nextButton: Button
+//    private lateinit var prevButton: Button
+
+    //    Challenge: From Button to ImageButton
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -29,12 +33,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
@@ -50,31 +54,24 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        //Challenge: Add a Previous Button
+        prevButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
+            //when on first question wrap back to last question
+            if (currentIndex < 0)
+                currentIndex = questionBank.size-1
+            updateQuestion()
+        }
+
+        //Challenge: Add a Listener to the TextView
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
         updateQuestion()
 
     }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume() called")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
-    }
-
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
