fc2f8d2c33a4fadef10b32bc5cc1398b99182c21
Moina Vernon <mvernon@cpp.edu>
2022-09-06 21:45:08 -0700

    Listing 4.7

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizViewModel.kt
@@ -16,4 +16,12 @@ class QuizViewModel : ViewModel() {
         Question(R.string.question_asia, true)
     )
 
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
 }
